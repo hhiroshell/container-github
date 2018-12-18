7ddc896ac5a56a212be352065831e6bdd1dcafbc
hhiroshell <hhiroshell@gmail.com>
2018-12-19 01:33:06 +0900

    test

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mkdir -p "$HOME/.config"
+ls -a "$HOME"
 touch "$HOME/.config/git-credential"
 touch "$HOME/.config/hub"
 echo "https://$WERCKER_GITHUB_TOKEN:@github.com" > "$HOME/.config/git-credential"
